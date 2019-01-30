3d042a7c0dc99fe0ecd57c96e73b305a1ca22444
yn smol <ynis1998@gmail.com>
2019-01-30 21:03:44 +0300

    Java oop

--- src/main/java/ua/edu/chmnu/fks/oop/lab02/Lab02.java
+++ src/main/java/ua/edu/chmnu/fks/oop/lab02/Lab02.java
@@ -14,22 +14,43 @@ import java.util.Scanner;
 public class Lab02 {
       
     public static void main(String[] args){
-        /**
+
         Scanner scan = new Scanner(System.in);
-        double y = 0;
-         */
-        System.out.println("Просчитать функцию со случайными параметрами: ");
+
+        System.out.println("1. Просчитать функцию с заданными параметрами: ");
+        System.out.println("2. Просчитать функцию со случайными параметрами: ");
+        double a = 4.16, b = 5.07, x = -6.31;
+        double y1, y2;
+        double aa = Math.random() * 20 + (-10);
+        double bb = Math.random() * 20 + (-10);
+        double xx = Math.random() * 20 + (-10);
+
+        y1 = function (a, b, x);
+        y2 = function(aa, bb, xx);
 
 
+        while (Math.pow(Math.exp((Math.cos(2/1-x)+ bb * Math.sin(3*x))), aa * x) < 0){
+            bb = Math.random() * 20 + (-10);
+            aa = Math.random() * 20 + (-10);
+        }
 
 
-
-
-        double a = 4.16, b = Math.random() * 20 + (-10), x = -6.31;
-        double j = Math.pow(Math.exp((Math.cos(2/1-x)+ b * Math.sin(3*x))), a * x);
-      
-      System.out.println("Ответ : " + j + "\n");
-
+        switch(scan.nextInt()){
+            case 1:
+                System.out.println("y = " + y1);
+                break;
+            case 2:
+                System.out.println("y = " + y2);
+                break;
+        }
       }
- 
+
+
+
+      static double function (double a, double b, double x){
+        return Math.pow(Math.exp((Math.cos(2/1-x)+ b * Math.sin(3*x))), a * x);
+      }
 }
+
+
+//Math.random() * 20 + (-10)
