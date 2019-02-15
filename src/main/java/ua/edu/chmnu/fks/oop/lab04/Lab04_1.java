e9696d28031c8a13737206075b8a389821873eba
yn smol <ynis1998@gmail.com>
2019-02-15 19:44:56 +0300

    Java oop

--- src/main/java/ua/edu/chmnu/fks/oop/lab04/Lab04_1.java
+++ src/main/java/ua/edu/chmnu/fks/oop/lab04/Lab04_1.java
@@ -4,20 +4,28 @@ import java.math.BigInteger;
 
 public class Lab04_1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-      System.out.println(fact(5));
+        System.out.println("Answer is " + fact(5));
     }
 
 
-    public static int fact (int n){
+    public static int fact(int n) {
 
         if (n == 1)
             return 1;
 
-
-        return n * fact(n-1);
-
+        return n * fact(n - 1);
     }
 
+    public static BigInteger fact(BigInteger n) {
+
+        if (n.equals(BigInteger.ONE))
+            return BigInteger.ONE;
+
+        return n.multiply(fact(n.subtract(BigInteger.ONE)));
+
+    }
 }
+
+
